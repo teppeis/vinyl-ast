6b087abfd5d662633b35a3663de30a97d776de57
teppeis <teppeis@gmail.com>
2014-11-03 02:23:18 +0900

    Add apply method instead of subclass

--- test/index.js
+++ test/index.js
@@ -4,68 +4,50 @@ var assert = require('power-assert');
 var esprima = require('esprima');
 var Vinyl = require('vinyl');
 
-var VinylAst = require('../index');
+var vinylAst = require('../index');
 
-describe('VinylAst', function() {
-    describe('constructor()', function() {
-        it('should instantiate', function() {
-            assert(new VinylAst() instanceof VinylAst);
-        });
+describe('vinylAst', function() {
+    var sut, buf;
+    beforeEach(function() {
+        sut = new Vinyl();
+        buf = new Buffer('foo');
+        sut.contents = buf;
+        sut.customProp = 'bar';
 
-        it('should inherits Vinyl', function() {
-            var sut = new VinylAst();
+        vinylAst.apply(sut);
+    });
+
+    describe('#apply()', function() {
+        it('should keep original inheritence', function() {
             assert(sut instanceof Vinyl);
-        });
-
-        it('should give the args to Vinyl', function() {
-            var buf = new Buffer('foo');
-            var sut = new VinylAst({contents: buf});
-            assert(sut.contents === buf);
             assert(sut.isBuffer());
+            assert(sut.contents === buf);
+            assert(sut.customProp === 'bar');
+        });
+
+        it('#clone');
+
+        it('should set null to #ast', function() {
+            assert(sut.ast === null);
         });
     });
 
-    context('#ast is not given', function() {
-        var sut, buf;
-        beforeEach(function() {
-            sut = new VinylAst();
-            buf = new Buffer('foo');
-            sut.contents = buf;
-        });
-
-        describe('#ast', function() {
-            it('should be null', function() {
-                assert(sut.ast === null);
-            });
-        });
-
-        describe('#contents', function() {
-            it('should be the given buffer', function() {
-                assert(sut.contents === buf);
-            });
-
-            it('should be null after #ast is modified', function() {
-                assert(sut.contents === buf);
-            });
-        });
-    });
-
-    context('#ast is given', function() {
-        var sut, src, ast;
+    context('when #ast is assigned', function() {
+        var src, ast;
         beforeEach(function() {
             src = "var foo = 'foo';";
             ast = esprima.parse(src);
-            sut = new VinylAst({ast: ast});
+            sut.ast = ast;
         });
 
         describe('#ast', function() {
-            it('should be set by constructor', function() {
+            it('should be assigned ast', function() {
                 assert(sut.ast === ast);
             });
 
-            it('should be null after #contents is modified', function() {
-                sut.contents = new Buffer('foo');
-                assert(sut.ast === null);
+            it('should be assigned ast after getting #contents', function() {
+                assert(sut.contents instanceof Buffer);
+                assert(sut.ast === ast);
             });
         });
 
@@ -81,32 +63,8 @@ describe('VinylAst', function() {
                 assert(sut.isBuffer());
             });
         });
+    });
 
-        context('#astSource is given', function() {
-            var astSource;
-            beforeEach(function() {
-                astSource = "var src = 'awsome!';";
-                sut.astSource = astSource;
-            });
-
-            describe('#contents', function() {
-                it('should be specified astSource', function() {
-                    assert(sut.contents instanceof Buffer);
-                    assert(sut.contents.toString() === astSource);
-                });
-            });
-
-            describe('#astSource', function() {
-                it('should be null after #ast is modified', function() {
-                    sut.ast = {};
-                    assert(sut.astSource === null);
-                });
-
-                it('should be null after #contents is modified', function() {
-                    sut.contents = new Buffer('foo');
-                    assert(sut.astSource === null);
-                });
-            });
-        });
+    context('apply twice', function() {
     });
 });
